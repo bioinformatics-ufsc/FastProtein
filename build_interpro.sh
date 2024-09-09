c3538a4c37596e75aa55224b4df99fe1bb58d4bf
simoesrenato <renatosm@gmail.com>
2024-09-08 23:23:38 -0300

    Updating docker label

--- build_interpro.sh
+++ build_interpro.sh
@@ -1,2 +1,2 @@
-docker build -t bioinfoufsc/proteomic:interpro -f Dockerfile-proteomic-interpro .
-docker build -t bioinfoufsc/fastprotein:interpro -f Dockerfile-interpro .
+docker build -t bioinfoufsc/proteomic-interpro:latest -f Dockerfile-proteomic-interpro .
+docker build -t bioinfoufsc/fastprotein-interpro:latest -f Dockerfile-interpro .
--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 docker push bioinfoufsc/proteomic:latest
 docker push bioinfoufsc/fastprotein:latest
-docker push bioinfoufsc/proteomic:interpro
-docker push bioinfoufsc/fastprotein:interpro
+docker push bioinfoufsc/proteomic-interpro:latest
+docker push bioinfoufsc/fastprotein-interpro:latest
 
 
