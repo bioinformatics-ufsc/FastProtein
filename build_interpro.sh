ad024335d019bc0696a758032f3bbf034a9c4117
simoesrenato <renatosm@gmail.com>
2024-09-11 01:58:41 -0300

    including tag bioinfoufsc/fastprotein:interpro

--- build_interpro.sh
+++ build_interpro.sh
@@ -1,2 +1,3 @@
 docker build -t bioinfoufsc/proteomic-interpro:latest -f Dockerfile-proteomic-interpro .
 docker build -t bioinfoufsc/fastprotein-interpro:latest -f Dockerfile-interpro .
+docker tag bioinfoufsc/fastprotein-interpro:latest bioinfoufsc/fastprotein:interpro
--- push.sh
+++ push.sh
@@ -2,5 +2,6 @@ docker push bioinfoufsc/proteomic:latest
 docker push bioinfoufsc/fastprotein:latest
 docker push bioinfoufsc/proteomic-interpro:latest
 docker push bioinfoufsc/fastprotein-interpro:latest
+docker push bioinfoufsc/fastprotein:interpro
 
 
